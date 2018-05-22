7a9589b8d8ce17a9bda19d0ed4b3c6f2c4b3cf12
Eduardo Rodriguez <erodriguez2@miuandes.cl>
2018-05-22 12:32:26 -0400

    Mostrar notificacion OK.

--- app/src/main/java/eduardo/lab7/AlarmReceiver.kt
+++ app/src/main/java/eduardo/lab7/AlarmReceiver.kt
@@ -1,13 +1,33 @@
 package eduardo.lab7
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.content.BroadcastReceiver
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.TaskStackBuilder
 
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.d("ALARM", "AAAAAAAAAAAAAAAAAH")
+
+        GenerateNotification(context)
+    }
+
+    private fun GenerateNotification(context: Context) {
+        val stackBuilder = TaskStackBuilder.create(context)
+
+        val builder = NotificationCompat.Builder(context, "channelId")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Nueva Notificacion")
+                .setContentText("Contenido")
+
+        builder.setAutoCancel(true)
+
+        val mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        mNotificationManager.notify(0, builder.build())
     }
 }
